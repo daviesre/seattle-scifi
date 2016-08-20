05153349ccd62347af43df14c4d73c663d527a90
RubyBe <sidbenavente@hotmail.com>
2016-08-20 09:46:43 -0700

    add base directory structure, files, and setup content

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+// import Component code from angulary library
+import { Component } from 'angular2/core';
+
+// annotation
+@Component({
+  selector: 'my-app',
+  template: `<h1>Base Angular2 App</h1>`
+})
+
+// class declaration
+export class AppComponent {
+
+}
