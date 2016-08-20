a3fc94b6eedd59f9b2d30ed2b21e76dbb287df23
RubyBe <sidbenavente@hotmail.com>
2016-08-20 10:55:59 -0700

    finalize setup

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,9 +4,12 @@ import { Component } from 'angular2/core';
 // annotation
 @Component({
   selector: 'my-app',
-  template: `<h1>Base Angular2 App</h1>`
+  template: `
+    <div class = "container">
+      <h1>Skeleton Angular2 App!</h1>
+    </div>
+  `
 })
-
 // class declaration
 export class AppComponent {
 
