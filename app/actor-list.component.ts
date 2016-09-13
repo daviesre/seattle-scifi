5b457b6f5ccc6ea51146eff1b839356a30e7bd2f
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-09-13 16:22:28 -0700

    add edit component for actors

--- app/actor-list.component.ts
+++ app/actor-list.component.ts
@@ -4,24 +4,28 @@ import { ActorComponent } from './actor.component';
 import { AddActorComponent } from './add-actor.component';
 import { ActorService } from './actor.service';
 import { OnInit } from 'angular2/core';
+import { EditActorDetailsComponent } from './edit-actor-details.component';
 //import update later
 
 @Component({
   selector: 'actor-list',
   inputs: ['actorList'],
-  directives: [ActorComponent, AddActorComponent], //update directive needed later
+  directives: [ActorComponent, AddActorComponent, EditActorDetailsComponent], //update directive needed later
   template:
   `
   <p>Hello</p>
   <actor-display *ngFor="#currentActor of actorList"
+    (click)="actorClicked(currentActor)"
     [actor]="currentActor">
   </actor-display>
+  <edit-actor-details *ngIf="selectedActor" [actor]="selectedActor"></edit-actor-details>
   <add-actor (onSubmitAddActor)="createActor($event)"></add-actor>
   `,
   providers: [ActorService]
 })
 export class ActorListComponent {
   public actorList: Actor[];
+  public selectedActor: Actor;
 
   constructor(private actorService: ActorService) {
 
@@ -29,6 +33,10 @@ export class ActorListComponent {
 
   ngOnInit() {
     this.actorList = this.actorService.getActors();
+  }
+
+  actorClicked(clickedActor: Actor): void {
+    this.selectedActor = clickedActor;
   }
 
   //update method needed later
