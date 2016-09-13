fde1d693cbeacb89f4bf260b00fe8dddb3abd447
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-09-13 11:47:18 -0700

    update actor-list component to receive add event emitter, and update actor model to reflect updated json format

--- app/actor-list.component.ts
+++ app/actor-list.component.ts
@@ -1,22 +1,33 @@
 import { Component } from 'angular2/core'
 import { Actor } from './actor.model';
 import { ActorComponent } from './actor.component';
-//import edit later
-//import new later
+import { AddActorComponent } from './add-actor.component';
+//import update later
 
 @Component({
   selector: 'actor-list',
   inputs: ['actorList'],
-  directives: [ActorComponent], //create and update directives needed later
+  directives: [ActorComponent, AddActorComponent], //update directive needed later
   template:
   `
   <actor-display *ngFor="#currentActor of actorList"
     (click)="actorClicked(currentActor)"
     [actor]="currentActor">
   </actor-display>
+  <add-actor (onSubmitAddActor)="createActor($event)"></add-actor>
   `
 })
 export class ActorListComponent {
   public actorList: Actor[];
-  //create and update methods needed later
+  //update method needed later
+  createActor(actor) : void {
+    this.actorList.push(
+      new Actor(
+        actor.id,
+        actor.firstName,
+        actor.lastName,
+        actor.role,
+        actor.movie)
+    );
+  }
 }
