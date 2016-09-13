8f90970807c4ebbf65a1c634f976c7c33ee54a2a
Joel <joeltwaage@gmail.com>
2016-09-13 12:35:40 -0700

    revert movie-list.component and movie.component

--- app/movie-list.component.ts
+++ app/movie-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter } from 'angular2/core';
+import { Component } from 'angular2/core';
 import { MovieComponent } from './movie.component';
 import { Movie } from './movie.model';
 
@@ -8,19 +8,16 @@ import { Movie } from './movie.model';
   outputs: ['onMovieSelect'],
   directives: [MovieComponent],
   template: `
-    <movie-display>hello
+    <movie-display *ngFor="currentMovie of movieList"
+      (click)="movieClicked(currentMovie)"
+      [class.selected]="currentMovie === selectedMovie"
+      [movie]="currentMovie">
     </movie-display>
   `
 })
-export class MovieListComponent {
-  public movies: Movie[];
-  public onMovieSelect: EventEmitter<Movie>;
-  public selectedMovie: Movie;
+export class MovieLIstComponent {
+  public moives: Movie[];
   constructor() {
-    this.onMovieSelect = new EventEmitter();
-  }
-  movieClicked(clickedMovie: Movie): void {
-    this.selectedMovie = clickedMovie;
-    this.onMovieSelect.emit(clickedMovie);
+
   }
 }
