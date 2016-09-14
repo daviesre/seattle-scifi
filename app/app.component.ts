135a3edcdc01d78759ea37bb886bab3d35f388c1
Joel <joeltwaage@gmail.com>
2016-09-14 10:55:20 -0700

    refactor movie-list, actor-list, and quote-list components into new component called 'collected-list.component'

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,19 @@
 // import Component code from angulary library
 import { Component } from 'angular2/core';
 import { Movie } from "./movie.model";
-import { MovieListComponent } from "./movie-list.component";
+import { CollectedListComponent } from "./collected-list.component";
 
 // annotation
 @Component({
   selector: 'my-app',
-  directives: [MovieListComponent],
+  directives: [CollectedListComponent],
   template: `
     <div class = "container">
       <h1>Seattle Sci-Fi!</h1>
     </div>
 
     <div class="container">
-      <movie-list></movie-list>
+      <collected-list></collected-list>
     </div>
   `
 })
