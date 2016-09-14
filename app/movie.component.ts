222304f74d081b3bb27a4a30ce593befc48ee3c2
Russ Davies <russdavies392@gmail.com>
2016-09-14 11:55:31 -0700

    format movie list

--- app/movie.component.ts
+++ app/movie.component.ts
@@ -5,10 +5,16 @@ import { Component } from 'angular2/core';
   selector: 'movie-display',
   inputs: ['movie'],
   template: `
-    <h3>{{movie.id}}: {{ movie.title }}</h3>
-    <h2><img height="200" width="auto" src={{movie.image}}></h2>
-    <h4>directed by: {{movie.director}}</h4>
-    <h4>{{movie.description}} ({{movie.year}})</h4>
+    <div class="container">
+      <div class="movie-id">
+        <h3>{{movie.id}}: {{ movie.title }}</h3>
+        <h2><img height="200" width="auto" src={{movie.image}}></h2>
+      </div>
+      <div class="movie-description">
+        <h4>directed by: {{movie.director}}</h4>
+        <h4>{{movie.description}} ({{movie.year}})</h4>
+      </div>
+    </div>
   `
 })
 export class MovieComponent {
